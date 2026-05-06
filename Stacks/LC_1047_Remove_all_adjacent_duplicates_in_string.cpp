bed98ccfa2bcaf23db3e22b561e4ef699194f520
Shivani Singh <shivanis0339@gmail.com>
2026-05-07 00:39:06 +0530

    Add solution for removing adjacent duplicates in string
    Implement a solution to remove all adjacent duplicates from a string.

--- Stacks/LC_1047_Remove_all_adjacent_duplicates_in_string.cpp
+++ Stacks/LC_1047_Remove_all_adjacent_duplicates_in_string.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int len=s.size();
+        while(true){
+            bool found=false;
+            for(int i=0;i<len-1;i++){
+                if(s[i]==s[i+1]){
+                    s.erase(i,2);
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+                break;
+            }
+            
+        }
+        return s;
+    }
+};
